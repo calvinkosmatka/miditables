949cba70c151fb78c8c197bd7b743a468b47a676
Calvin Kosmatka <calvin.kosmatka@gmail.com>
2020-11-04 19:21:38 -0600

    evtype matcher working

--- src/matchers/evtype.rs
+++ src/matchers/evtype.rs
@@ -1,21 +1,39 @@
 use crate::matcher::Matcher;
-use alsa::seq;
+use std::collections::HashSet;
+use alsa::seq::{self, EventType};
 
 pub struct EvTypeMatcher {
-
+    types_to_match: HashSet<EventType>,
 }
 
 impl Matcher for EvTypeMatcher {
     fn parse_args(&mut self, args: Vec<String>) {
-
+        for evtype in args.iter() {
+            match evtype.as_ref() {
+                "noteon" => {
+                    self.types_to_match.insert(EventType::Noteon);
+                },
+                "noteoff" => {
+                    self.types_to_match.insert(EventType::Noteoff);
+                },
+                "note" => {
+                    self.types_to_match.insert(EventType::Note);
+                },
+                x => {
+                    println!("evtypematcher {}", x);
+                }
+            }
+        }
     }
     fn r#match(&mut self, event: &seq::Event) -> bool {
-        true
+        self.types_to_match.contains(&event.get_type())
     }
 }
 
 impl EvTypeMatcher {
     pub fn new() -> EvTypeMatcher {
-        EvTypeMatcher {}
+        EvTypeMatcher {
+            types_to_match: HashSet::new()
+        }
     }
 }
