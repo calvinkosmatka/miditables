2dfaa2dad729945bd1e9bf135128c8dacae9c917
Calvin Kosmatka <calvin.kosmatka@gmail.com>
2020-11-04 22:11:11 -0600

    remove some commented code

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,6 @@ fn main() {
     let mut c = Config::new("conf/miditables.conf");
     let (table, mut input, output) = c.parse();
     let table = Arc::new(table);
-    //let client = Mutex::new(SeqClient::new(c.ins, c.outs));
-    //let mut sinput = client.get_input();
-    //let thread_client = &client;
-    //thread::spawn(move || {
-    //    let data = alsa::seq::EvNote::default();
-    //    let mut x = alsa::seq::Event::new(alsa::seq::EventType::Noteon, &data);
-    //    thread_client.output_event(&mut x);
-    //});
-
     let (tx, rx) = channel();
 
     thread::spawn(move || {
